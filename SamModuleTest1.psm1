89247226ada4234ae2e68bd738dc70e9045b0c8b
chinna456 <sainathchary9337@gmail.com>
2020-08-30 20:45:19 +0530

    Add files via upload

---
+++ SamModuleTest1.psm1
@@ -0,0 +1,12 @@
+﻿#sample module
+function Say-Hello{
+# .ExternalHelp Microsoft.Xrm.Data.PowerShell.Help.xml
+    [CmdletBinding()]
+    PARAM(
+        [parameter(Mandatory=$false)]
+        [string]$Name
+    )
+
+    Write-host "Hello, $Name";
+    return $Name+"Return";
+ }
